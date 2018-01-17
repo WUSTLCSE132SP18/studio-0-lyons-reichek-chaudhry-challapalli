5bcd2f7da2715108c836c763fd973dbb532385a2
maheshchallapalli <maheshchallapalli@maheshs-air.dhcp.wustl.edu>
2018-01-17 17:10:23 -0600

    fixed this

--- src/Heartbeat.java
+++ src/Heartbeat.java
@@ -2,12 +2,12 @@
 public class Heartbeat {
 
 	public static void main(String[] args) {
-		int time = 0;
+		float time = 0;
 		while(true){
 			
 			try{
 				Thread.sleep(1000);
-				time += 1;
+				time += System.currentTimeMillis() / 1000F;
 				System.out.println(time + " sec have elapsed");
 			}
 			catch(InterruptedException e){
