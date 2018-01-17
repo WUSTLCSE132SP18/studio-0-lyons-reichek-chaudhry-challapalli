65e1df96ae1fce19ea13ae3157e654255e0bfd7c
maheshchallapalli <maheshchallapalli@maheshs-air.dhcp.wustl.edu>
2018-01-17 16:59:42 -0600

    yay

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -1,9 +1,12 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(13, LOW);
   delay(1000);
+  digitalWrite(13, HIGH);
   int time = (millis())/1000;
   Serial.print(time);
   Serial.println(" sec have elapsed");
