9b157dba811b1815351049d9ceeaf84f1515a1fc
maheshchallapalli <maheshchallapalli@maheshs-air.dhcp.wustl.edu>
2018-01-17 17:07:23 -0600

    i am done with this

--- src/Heartbeat.java
+++ src/Heartbeat.java
@@ -2,11 +2,15 @@
 public class Heartbeat {
 
 	public static void main(String[] args) {
+		int time = 0;
 		while(true){
+			
 			try{
 				Thread.sleep(1000);
+				time += 1;
+				System.out.println(time + " sec have elapsed");
 			}
-			catch(InterruptedException){
+			catch(InterruptedException e){
 				
 			}
 		}
