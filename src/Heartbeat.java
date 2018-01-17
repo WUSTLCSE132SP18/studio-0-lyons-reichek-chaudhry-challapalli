76c5479e22f20354b638b011fa027ff05d37d74f
TobyL <lyons.t@wustl.edu>
2018-01-17 16:59:33 -0600

    java implementation

---
+++ src/Heartbeat.java
@@ -0,0 +1,16 @@
+
+public class Heartbeat {
+
+	public static void main(String[] args) {
+		while(true){
+			try{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException){
+				
+			}
+		}
+
+	}
+
+}
